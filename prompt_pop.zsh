527c6637f218384ffd35ddb4e010534e5b725c43
Miles <miles@karmacheck.com>
2023-11-06 11:43:08 -0600

    this is broken and needs to be examined

--- prompt_pop.zsh
+++ prompt_pop.zsh
@@ -1,26 +1,77 @@
+# prompt_pop() {
+#   local typed_command=$BUFFER
+#   local log_file="/tmp/prompt_pop.log" # Define log file path
+# 
+#   echo "Starting prompt_pop function" >> $log_file
+#   echo "Buffer contains: $typed_command" >> $log_file
+# 
+#   if [[ -z $PROMPT_POP_FILE ]]; then
+#     PROMPT_POP_FILE="/tmp/prompt_pop.zsh"
+#     echo "PROMPT_POP_FILE was not set. Using default: $PROMPT_POP_FILE" >> $log_file
+#   else
+#     echo "PROMPT_POP_FILE is set to: '$PROMPT_POP_FILE'" >> $log_file
+#   fi
+# 
+#   echo "$typed_command" > "$PROMPT_POP_FILE"
+# 
+#   if [[ -z $PROMPT_POP_COMMAND ]]; then
+#     PROMPT_POP_COMMAND="tmux display-popup -E -w 80% -h 80% 'nvim \"$PROMPT_POP_FILE\"'"
+#     echo "PROMPT_POP_COMMAND was not set. Using default: $PROMPT_POP_COMMAND" >> $log_file
+#   else
+#     # Make sure to escape inner quotes correctly
+#     PROMPT_POP_COMMAND=${PROMPT_POP_COMMAND//'\$PROMPT_POP_FILE'/"$PROMPT_POP_FILE"}
+#     echo "PROMPT_POP_COMMAND is set to: '$PROMPT_POP_COMMAND'" >> $log_file
+#   fi
+# 
+#   echo "Executing command: $PROMPT_POP_COMMAND" >> $log_file
+#   eval "$PROMPT_POP_COMMAND"
+# 
+#   BUFFER=$(<"$PROMPT_POP_FILE")
+#   CURSOR=$#BUFFER
+#   echo "Buffer updated. New buffer: $BUFFER" >> $log_file
+#   echo "New cursor position: $CURSOR" >> $log_file
+# 
+#   zle reset-prompt
+#   echo "Prompt reset" >> $log_file
+# }
+# 
+# zle -n prompt_pop
+#
 prompt_pop() {
   local typed_command=$BUFFER
+  local log_file="/tmp/prompt_pop.log" # Define log file path
 
-  # Check if PROMPT_POP_FILE is not set or empty and export a default value if necessary
-  if [[ -z "${PROMPT_POP_FILE}" ]]; then
-    export PROMPT_POP_FILE="/tmp/prompt_pop.zsh"
-  fi
+  echo "Starting prompt_pop function" >> $log_file
+  echo "Buffer contains: $typed_command" >> $log_file
 
-  local temp_file=$PROMPT_POP_FILE
-
-  echo $typed_command > $temp_file
-  
-  # Use the environment variable for the command, ensuring it falls back to a default if not set.
-  #
-  if [[ -n $PROMPT_POP_COMMAND ]]; then
-    eval "$PROMPT_POP_COMMAND"
+  if [[ -z $PROMPT_POP_FILE ]]; then
+    PROMPT_POP_FILE="/tmp/prompt_pop.zsh"
+    echo "PROMPT_POP_FILE was not set. Using default: $PROMPT_POP_FILE" >> $log_file
   else
-    nvim "$temp_file"
+    echo "PROMPT_POP_FILE is set to: '$PROMPT_POP_FILE'" >> $log_file
   fi
 
-  BUFFER=$(< $temp_file)
+  echo "$typed_command" > "$PROMPT_POP_FILE"
+
+  if [[ -z $PROMPT_POP_COMMAND ]]; then
+    PROMPT_POP_COMMAND="tmux display-popup -E -w 80% -h 80% 'nvim \"$PROMPT_POP_FILE\"'"
+    echo "PROMPT_POP_COMMAND was not set. Using default: $PROMPT_POP_COMMAND" >> $log_file
+  else
+    # Make sure to escape inner quotes correctly
+    PROMPT_POP_COMMAND=${PROMPT_POP_COMMAND//'\$PROMPT_POP_FILE'/"$PROMPT_POP_FILE"}
+    echo "PROMPT_POP_COMMAND is set to: '$PROMPT_POP_COMMAND'" >> $log_file
+  fi
+
+  echo "Executing command: $PROMPT_POP_COMMAND" >> $log_file
+  eval "$PROMPT_POP_COMMAND"
+
+  BUFFER=$(<"$PROMPT_POP_FILE")
   CURSOR=$#BUFFER
+  echo "Buffer updated. New buffer: $BUFFER" >> $log_file
+  echo "New cursor position: $CURSOR" >> $log_file
+
   zle reset-prompt
+  echo "Prompt reset" >> $log_file
 }
 
 zle -N prompt_pop

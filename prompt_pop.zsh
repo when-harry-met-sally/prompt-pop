05db13462b057498ca8c39563636c537e418abc2
when-harry-met-sally <milesmoran.mu@gmail.com>
2024-08-31 16:21:58 -0500

    reverted

--- prompt_pop.zsh
+++ prompt_pop.zsh
@@ -2,29 +2,24 @@ prompt_pop() {
   local typed_command=$BUFFER
   local log_file="/tmp/prompt_pop.log" # Define log file path
 
-  echo "Starting prompt_pop function" >> "$log_file"
-  echo "Buffer contains: $typed_command" >> "$log_file"
+  echo "Starting prompt_pop function" >> $log_file
+  echo "Buffer contains: $typed_command" >> $log_file
 
   local PROMPT_POP_FILE="/tmp/prompt_pop.sh"
 
   echo "$typed_command" > "$PROMPT_POP_FILE"
 
-  # Use Neovim to edit the command and automatically save and quit
-  nvim -c "setlocal buftype=nofile | setlocal bufhidden=hide" -c "wq" "$PROMPT_POP_FILE"
+  nvim "$PROMPT_POP_FILE"
 
-  # Read the modified command back into the buffer
   BUFFER=$(<"$PROMPT_POP_FILE")
   CURSOR=$#BUFFER
+  echo "Buffer updated. New buffer: $BUFFER" >> $log_file
+  echo "New cursor position: $CURSOR" >> $log_file
 
-  echo "Buffer updated. New buffer: $BUFFER" >> "$log_file"
-  echo "New cursor position: $CURSOR" >> "$log_file"
+  tmux send-keys Enter
 
-  # Uncomment the next line if you want to automatically send Enter in tmux
-  # tmux send-keys Enter
-
-  # Refresh the Zsh prompt
   zle reset-prompt
-  echo "Prompt reset" >> "$log_file"
+  echo "Prompt reset" >> $log_file
 }
 
 zle -N prompt_pop
